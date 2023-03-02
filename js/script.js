1ba7d9232dc96c805612fad2256eed38af1b6da8
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2023-03-02 08:34:02 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -32,3 +32,161 @@ var swiper = new Swiper(".slide-content", {
   });
   
   
+  function lift1(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/lift1.jpg";  
+  
+  }
+  
+  function lift2(){
+  
+  document.getElementById('myImage1')
+  
+  .src="images/lift2.jpg";  
+  
+  }
+ 
+  
+  function hd1(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/hydraulic1.jpg";  
+  
+  }
+  
+  function hd2(){
+  
+  document.getElementById('myImage2')
+  
+  .src="images/hydraulic2.jpg";  
+  
+  }
+  
+  function apfc1(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/apfc2.jpg";  
+  
+  }
+  
+  function apfc2(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/apfc3.jpg";  
+  
+  }
+  
+  function apfc3(){
+  
+  document.getElementById('myImage3')
+  
+  .src="images/apfc.jpeg";  
+  
+  }
+  
+  function meter1(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/meter1.jpg";  
+  
+  }
+  
+  function meter2(){
+  
+  document.getElementById('myImage4')
+  
+  .src="images/meter2.jpg";  
+  
+  }
+  
+    
+  function feed1(){
+  
+  document.getElementById('myImage5')
+  
+  .src="images/feeder1.jpg";
+  
+  }
+  
+  function feed2(){
+  
+  document.getElementById('myImage5')
+  
+  .src="images/feeder2.jpg";
+  
+  
+  }
+  
+  function feed3(){
+  
+  document.getElementById('myImage5')
+  
+  .src="images/feeder3.jpg";  
+  
+  }
+  
+  function flp1(){
+  
+  document.getElementById('myImage6')
+  
+  .src="images/flp3.jpg";
+  
+  }
+  
+  function flp2(){
+  
+  document.getElementById('myImage6')
+  
+  .src="images/flpremote1.jpg";
+  
+  
+  }
+  
+  function flp3(){
+  
+  document.getElementById('myImage6')
+  
+  .src="images/flp4.jpg";  
+  
+  }
+  
+  function flp4(){
+  
+  document.getElementById('myImage6')
+  
+  .src="images/flp2.jpg";
+  
+  }
+  
+  
+  function vfd1(){
+  
+  document.getElementById('myImage7')
+  
+  .src="images/vfd2.jpg";  
+  
+  }
+  
+  function vfd2(){
+  
+  document.getElementById('myImage7')
+  
+  .src="images/vfd1.jpg";  
+  
+  }
+  
+  function vfd3(){
+  
+  document.getElementById('myImage7')
+  
+  .src="images/vfd3.jpg";  
+  
+  }
+  
+  
