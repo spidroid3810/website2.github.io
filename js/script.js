feb35051797b9195e9dc2339796e84c6f2da10c1
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2023-03-03 13:32:42 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -189,4 +189,25 @@ var swiper = new Swiper(".slide-content", {
   
   }
   
-  
+  $(".carousel").owlCarousel({
+            margin: 20,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            responsive: {
+            0:{
+            items:1,
+            nav: false
+            },
+            600:{
+            items:2,
+            nav: false
+            },
+            1000:{
+            items:3,
+            nav: false
+            }
+            }
+            });
+    
